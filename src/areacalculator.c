a204a17a4634f2c433c0f242fa22e4e167bc9c05
Dan Banfield <dandbanfield@gmail.com>
2022-07-06 20:40:31 +0100

    Updated areacalculator.c.
    Added a base for option selection.

--- src/areacalculator.c
+++ src/areacalculator.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <Windows.h>
 
+void getoptionselection()
+{
+    printf("Select an option: ");
+
+    int selectedIndex = 1;
+    scanf("%d", &selectedIndex);
+
+    switch (selectedIndex) 
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    }
+}
+
+void showoptions()
+{
+    printf("1 - Square\n");
+    printf("2 - Rectangle\n");
+    printf("3 - Triangle\n\n");
+
+    getoptionselection();
+}
+
 void setconsolecolour(int colourCode)
 {
     HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -11,8 +38,8 @@ void setupconsole()
 {
     SetConsoleTitle("Area Calculator");
     setconsolecolour(15);
-    printf("Hello, world!");
-    getchar();
+
+    showoptions();
 }
 
 int main()
