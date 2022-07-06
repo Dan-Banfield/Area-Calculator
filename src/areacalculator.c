c02c69730f141416358e23de5ec4db7a428eb2f9
Dan Banfield <dandbanfield@gmail.com>
2022-07-06 20:34:01 +0100

    Updated areacalculator.c.

--- src/areacalculator.c
+++ src/areacalculator.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <Windows.h>
+
+void setconsolecolour(int colourCode)
+{
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(consoleHandle, colourCode);
+}
+
+void setupconsole()
+{
+    SetConsoleTitle("Area Calculator");
+    setconsolecolour(15);
+    printf("Hello, world!");
+    getchar();
+}
 
 int main()
 {
-    // TODO: Add code.
+    setupconsole();
     return 0;
 }
